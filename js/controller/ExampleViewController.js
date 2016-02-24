03983de52d7f156bd3d813ed9e8062458cdd4397
Clement <cle.hamon@gmail.com>
2016-02-24 11:29:12 +0100

    fix syntax exampleViewCtrl

--- js/controller/ExampleViewController.js
+++ js/controller/ExampleViewController.js
@@ -1,14 +1,13 @@
 var ExampleViewController = function (view, model){
 
-view.plusButton.click(function()){
-dinnerModel.setNumberOfGuests () + 1 ;
-}
+	view.plusButton.click(function(){
+		dinnerModel.setNumberOfGuests (model.getNumberOfGuests() - 1) ;
+	});
 
-view.minusButton.click(function(){
-model.setNumberOfGuests(model.getNumberOfGuests() - 1);
- });
-
-}
-
+	view.minusButton.click(function(){
+		model.setNumberOfGuests(model.getNumberOfGuests() - 1);
+	});
 
 }
+
+
