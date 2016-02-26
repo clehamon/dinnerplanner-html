81c91f572929260fd3e7f5094bb6195d1197f44d
yash434 <yashatlancs@gmail.com>
2016-02-26 13:14:32 +0100

    changed to view2.js

--- js/view/exampleView.js
+++ js/view/exampleView.js
@@ -1,16 +1,6 @@
 //ExampleView Object constructor
 var ExampleView = function (container, model) {
 	
-	// Get all the relevant elements of the view (ones that show data
-  	// and/or ones that responed to interaction)
-	this.numberOfGuests = container.find("#numberOfGuests");
-	this.plusButton = container.find("#plusGuest");
-	this.minusButton = container.find("#minusGuest");
-	
-
-	//Number of Guests
-	this.numberOfGuests.html(model.getNumberOfGuests());
-
 	init();
 
 	function init(){
--- js/view/view2.js
+++ js/view/view2.js
@@ -7,6 +7,21 @@ var View2 = function (container, model) {
 	this.sidebarItems = $("#sidebarItems");
 
 
+
+
+
+	// Get all the relevant elements of the view (ones that show data
+  	// and/or ones that responed to interaction)
+	this.numberOfGuests = container.find("#numberOfGuests");
+	this.plusButton = container.find("#plusGuest");
+	this.minusButton = container.find("#minusGuest");
+	
+
+	//Number of Guests
+	this.numberOfGuests.html(model.getNumberOfGuests());
+
+
+
 	$("#confirmDinner").click(function(){
 
 		// loadFinalMenuList();
